fb6b9d1bd7a73d374dab8e17cfdbab05343f1eba
Vladislav <zhurkin111@bk.ru>
2021-03-19 19:24:15 +0300

    add composer

--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <?php
-set_include_path('classes');
-spl_autoload_register();
+    require_once 'vendor/autoload.php';
+    use TaskForse\Task;
 
-$task = new Task(111, 222);
+    $task = new Task(111, 222);
 
-assert($task->getNextStatus('cancel') === Task::STATUS_CANCELED, 'cancel action');
-assert($task->getNextStatus('performed') === Task::STATUS_DONE, 'performed action');
-assert($task->getNextStatus('respond') === Task::STATUS_WORK, 'respond action');
-assert($task->getNextStatus('refuse') === Task::STATUS_FAILED, 'refuse action');
+    assert($task->getNextStatus('cancel') === Task::STATUS_CANCELED, 'cancel action');
+    assert($task->getNextStatus('performed') === Task::STATUS_DONE, 'performed action');
+    assert($task->getNextStatus('respond') === Task::STATUS_WORK, 'respond action');
+    assert($task->getNextStatus('refuse') === Task::STATUS_FAILED, 'refuse action');
 
-assert($task->getAvailableAction('new', 111) === Task::ACTION_RESPOND, 'progress status');
-assert($task->getAvailableAction('work', 111) === Task::ACTION_REFUSE, 'refuse status');
-assert($task->getAvailableAction('new', 222) === Task::ACTION_CANCEL, 'respond status');
-assert($task->getAvailableAction('work', 222) === Task::ACTION_PERFORMED, 'performed status');
+    assert($task->getAvailableAction('new', 111) === Task::ACTION_RESPOND, 'progress status');
+    assert($task->getAvailableAction('work', 111) === Task::ACTION_REFUSE, 'refuse status');
+    assert($task->getAvailableAction('new', 222) === Task::ACTION_CANCEL, 'respond status');
+    assert($task->getAvailableAction('work', 222) === Task::ACTION_PERFORMED, 'performed status');
--- src/Task.php
+++ src/Task.php
@@ -1,5 +1,7 @@
 <?php
 
+    namespace TaskForse;
+
     class Task
     {
 
